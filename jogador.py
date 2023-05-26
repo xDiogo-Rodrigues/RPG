771b51e6730eb638dea404658cfbff35bfb61781
xDiogo-Rodrigues <diogosouzak11@gmail.com>
2023-05-26 06:39:09 -0300

    Declarado algumas property e setter para os atributos

--- jogador.py
+++ jogador.py
@@ -4,3 +4,32 @@ class Jogador:
         self.__hp_player = hp_player
         self.__atack_player = atack_player
         self.__defense_player = defense_player
+
+    @property
+    def show_name_player(self):
+        return self.__name_player
+
+    @property
+    def show_hp_player(self):
+        return self.__hp_player
+    
+    @show_hp_player.setter
+    def __modify_hp_player(self, damage):
+        self.__hp_player -= damage
+
+    @property
+    def show_atack_player(self):
+        return self.__atack_player
+    
+    @show_atack_player.setter
+    def __modify_atack_player(self,value):
+        self.__atack_player += value
+    
+    @property
+    def show_defense_player(self):
+        return self.__defense_player
+    
+    @show_defense_player.setter
+    def __modify_defense_player(self,value):
+        self.__defense_player += value
+   
