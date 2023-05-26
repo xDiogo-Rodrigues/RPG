1749ca40a2aa72d8b4d79948b03554e19d239b91
xDiogo-Rodrigues <diogosouzak11@gmail.com>
2023-05-26 05:39:51 -0300

    Declarada a classe jogador e seu metodo construtor

---
+++ jogador.py
@@ -0,0 +1,6 @@
+class Jogador:
+    def __init__(self, name_player, hp_player, atack_player, defense_player):
+        self.__name_player = name_player
+        self.__hp_player = hp_player
+        self.__atack_player = atack_player
+        self.__defense_player = defense_player
